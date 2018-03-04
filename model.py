236dfb95efdcf6eeb0407981e434026f6fe60686
Qi_Yao <qy_upup@sina.com>
2018-03-04 17:34:17 +0800

    using Lenet for train

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import csv
 import cv2
 import numpy as np
+import matplotlib.image as mpimg
 
 #import the data from the csv file
 lines = []
@@ -12,11 +13,22 @@ with open('./data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
+path = './data/IMG/'
 for line in lines:
-    images_center = cv2.imread(line[0])
-    images.append(images_center)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    image_center = cv2.imread(path + line[0].split('/')[-1])
+    image_left = cv2.imread(path + line[0].split('/')[-1])
+    image_right = cv2.imread(path + line[0].split('/')[-1])
+    images.append(image_center)
+    images.append(image_left)
+    images.append(image_right)
+    measurement_center = float(line[3])
+    correction = 0.2
+    measurement_left = measurement_center + correction
+    measurement_right = measurement_center - correction
+    measurements.append(measurement_center)
+    measurements.append(measurement_left)
+    measurements.append(measurement_right)
+
 #Flipping Images And Steering Measurements
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -29,15 +41,28 @@ for image, measurement in zip(images, measurements):
 # y_train = np.array(measurements)
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
+print(X_train.shape)
+print(y_train.shape)
 #train the data
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+# model = Sequential()
+# model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3))) #normalizing the data and mean centering the data
+# model.add(Flatten())
+# model.add(Dense(1))
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3))) #normalizing the data and mean centering the data
+model.add(Cropping2D(cropping = ((70,25), (0,0)))) #remove the top 70 pixels and the bottom 25
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
